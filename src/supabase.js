6a648c7dc742e42ff14344d331d9816dcea59ff9
qwertyqwertys <ktmcmwkzpt@privaterelay.appleid.com>
2026-08-03 02:17:18 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://nilgxfmcwljqhawdrsot.co';
+const SUPABASE_URL = 'https://nilgxfmcwljqhawdrsot.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_K4ZLk0KkXTe8upyl7KoTcg_wD31DQ4U';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
